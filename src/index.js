0c285b48b5c81a9d0e30ad6dedba07c3fb2e2a60
Denis Kardash <dzianis.kardash@gmail.com>
2024-01-29 20:07:33 +0100

    Начало проекта

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { App } from './app';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+	// <React.StrictMode>
+		<App />
+	// </React.StrictMode>,
+);
+
+// <React.StrictMode> - выключим чтоб не "мешал" смотреть console (2-м выводом)
